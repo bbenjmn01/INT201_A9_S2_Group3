987807d1780ddb79a2e4573608ae0221a32a7d4f
ideastation-x <chumphu.phumin@gmail.com>
2021-11-17 20:56:02 +0700

    update CookieUtil

--- cookie.js
+++ cookie.js
@@ -1,24 +1,17 @@
 export class CookieUtil {
   static get(name) {
-    console.log(`all cookies: ${document.cookie}`);
     let cookieName = `${encodeURIComponent(name)}=`,
       cookieStart = document.cookie.indexOf(cookieName),
       cookieValue = null;
-    console.log(`cookieName = ${cookieName}`);
-    console.log(`cookieStart = ${cookieStart}`);
-
     if (cookieStart > -1) {
       let cookieEnd = document.cookie.indexOf(";", cookieStart);
-      console.log(`cookieEnd = ${cookieEnd}`);
       if (cookieEnd == -1) {
         cookieEnd = document.cookie.length;
       }
       cookieValue = decodeURIComponent(
         document.cookie.substring(cookieStart + cookieName.length, cookieEnd)
       );
-      console.log(`cookieValue = ${cookieValue}`);
     }
-
     return cookieValue;
   }
 
@@ -28,8 +21,6 @@ export class CookieUtil {
     if (expires instanceof Date) {
       cookieText += `; expires=${expires.toUTCString()}`;
     }
-
-    console.log(`cookieText = ${cookieText}`);
     document.cookie = cookieText;
   }
 
